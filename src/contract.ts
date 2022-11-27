69bc9380d1d8c8f0b3bb807cdad2eac2ac781d7b
Hung <hung@usaepay.com>
2022-11-27 07:29:28 -0800

    Initial commit

---
+++ src/contract.ts
@@ -0,0 +1,127 @@
+import {
+  Approval as ApprovalEvent,
+  ApprovalForAll as ApprovalForAllEvent,
+  ItemBought as ItemBoughtEvent,
+  ItemCanceled as ItemCanceledEvent,
+  ItemListed as ItemListedEvent,
+  MarketItemCreated as MarketItemCreatedEvent,
+  Transfer as TransferEvent
+} from "../generated/Contract/Contract"
+import {
+  Approval,
+  ApprovalForAll,
+  ItemBought,
+  ItemCanceled,
+  ItemListed,
+  MarketItemCreated,
+  Transfer
+} from "../generated/schema"
+
+export function handleApproval(event: ApprovalEvent): void {
+  let entity = new Approval(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.approved = event.params.approved
+  entity.tokenId = event.params.tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleApprovalForAll(event: ApprovalForAllEvent): void {
+  let entity = new ApprovalForAll(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.operator = event.params.operator
+  entity.approved = event.params.approved
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleItemBought(event: ItemBoughtEvent): void {
+  let entity = new ItemBought(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.buyer = event.params.buyer
+  entity.nftAddress = event.params.nftAddress
+  entity.tokenId = event.params.tokenId
+  entity.price = event.params.price
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleItemCanceled(event: ItemCanceledEvent): void {
+  let entity = new ItemCanceled(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.seller = event.params.seller
+  entity.nftAddress = event.params.nftAddress
+  entity.tokenId = event.params.tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleItemListed(event: ItemListedEvent): void {
+  let entity = new ItemListed(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.seller = event.params.seller
+  entity.nftAddress = event.params.nftAddress
+  entity.tokenId = event.params.tokenId
+  entity.price = event.params.price
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleMarketItemCreated(event: MarketItemCreatedEvent): void {
+  let entity = new MarketItemCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.tokenId = event.params.tokenId
+  entity.seller = event.params.seller
+  entity.owner = event.params.owner
+  entity.price = event.params.price
+  entity.sold = event.params.sold
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.tokenId = event.params.tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
---
+++ tests/contract-utils.ts
@@ -0,0 +1,208 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  Approval,
+  ApprovalForAll,
+  ItemBought,
+  ItemCanceled,
+  ItemListed,
+  MarketItemCreated,
+  Transfer
+} from "../generated/Contract/Contract"
+
+export function createApprovalEvent(
+  owner: Address,
+  approved: Address,
+  tokenId: BigInt
+): Approval {
+  let approvalEvent = changetype<Approval>(newMockEvent())
+
+  approvalEvent.parameters = new Array()
+
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromAddress(approved))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return approvalEvent
+}
+
+export function createApprovalForAllEvent(
+  owner: Address,
+  operator: Address,
+  approved: boolean
+): ApprovalForAll {
+  let approvalForAllEvent = changetype<ApprovalForAll>(newMockEvent())
+
+  approvalForAllEvent.parameters = new Array()
+
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromBoolean(approved))
+  )
+
+  return approvalForAllEvent
+}
+
+export function createItemBoughtEvent(
+  buyer: Address,
+  nftAddress: Address,
+  tokenId: BigInt,
+  price: BigInt
+): ItemBought {
+  let itemBoughtEvent = changetype<ItemBought>(newMockEvent())
+
+  itemBoughtEvent.parameters = new Array()
+
+  itemBoughtEvent.parameters.push(
+    new ethereum.EventParam("buyer", ethereum.Value.fromAddress(buyer))
+  )
+  itemBoughtEvent.parameters.push(
+    new ethereum.EventParam(
+      "nftAddress",
+      ethereum.Value.fromAddress(nftAddress)
+    )
+  )
+  itemBoughtEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+  itemBoughtEvent.parameters.push(
+    new ethereum.EventParam("price", ethereum.Value.fromUnsignedBigInt(price))
+  )
+
+  return itemBoughtEvent
+}
+
+export function createItemCanceledEvent(
+  seller: Address,
+  nftAddress: Address,
+  tokenId: BigInt
+): ItemCanceled {
+  let itemCanceledEvent = changetype<ItemCanceled>(newMockEvent())
+
+  itemCanceledEvent.parameters = new Array()
+
+  itemCanceledEvent.parameters.push(
+    new ethereum.EventParam("seller", ethereum.Value.fromAddress(seller))
+  )
+  itemCanceledEvent.parameters.push(
+    new ethereum.EventParam(
+      "nftAddress",
+      ethereum.Value.fromAddress(nftAddress)
+    )
+  )
+  itemCanceledEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return itemCanceledEvent
+}
+
+export function createItemListedEvent(
+  seller: Address,
+  nftAddress: Address,
+  tokenId: BigInt,
+  price: BigInt
+): ItemListed {
+  let itemListedEvent = changetype<ItemListed>(newMockEvent())
+
+  itemListedEvent.parameters = new Array()
+
+  itemListedEvent.parameters.push(
+    new ethereum.EventParam("seller", ethereum.Value.fromAddress(seller))
+  )
+  itemListedEvent.parameters.push(
+    new ethereum.EventParam(
+      "nftAddress",
+      ethereum.Value.fromAddress(nftAddress)
+    )
+  )
+  itemListedEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+  itemListedEvent.parameters.push(
+    new ethereum.EventParam("price", ethereum.Value.fromUnsignedBigInt(price))
+  )
+
+  return itemListedEvent
+}
+
+export function createMarketItemCreatedEvent(
+  tokenId: BigInt,
+  seller: Address,
+  owner: Address,
+  price: BigInt,
+  sold: boolean
+): MarketItemCreated {
+  let marketItemCreatedEvent = changetype<MarketItemCreated>(newMockEvent())
+
+  marketItemCreatedEvent.parameters = new Array()
+
+  marketItemCreatedEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+  marketItemCreatedEvent.parameters.push(
+    new ethereum.EventParam("seller", ethereum.Value.fromAddress(seller))
+  )
+  marketItemCreatedEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  marketItemCreatedEvent.parameters.push(
+    new ethereum.EventParam("price", ethereum.Value.fromUnsignedBigInt(price))
+  )
+  marketItemCreatedEvent.parameters.push(
+    new ethereum.EventParam("sold", ethereum.Value.fromBoolean(sold))
+  )
+
+  return marketItemCreatedEvent
+}
+
+export function createTransferEvent(
+  from: Address,
+  to: Address,
+  tokenId: BigInt
+): Transfer {
+  let transferEvent = changetype<Transfer>(newMockEvent())
+
+  transferEvent.parameters = new Array()
+
+  transferEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam(
+      "tokenId",
+      ethereum.Value.fromUnsignedBigInt(tokenId)
+    )
+  )
+
+  return transferEvent
+}
